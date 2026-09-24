a3de56220079fd2f84a61232c37779f35258824b
MD Nasim <nasimubd21@gmail.com>
2026-09-24 19:47:23 +0800

    chore(homebrew): update Argus to v1.24.0

--- Formula/argus.rb
+++ Formula/argus.rb
@@ -5,21 +5,21 @@ class Argus < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.2/argus_1.19.2_darwin_arm64.tar.gz"
-      sha256 "4507ee9c06ec6fe7de672cac93a9b442f7c5a37a12d771af0b271de1e41acce7"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.24.0/argus_1.24.0_darwin_arm64.tar.gz"
+      sha256 "c4dbf206b56ff3640a274b393365b8ca590c40fe0796929129f5b8e625c30e04"
     else
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.2/argus_1.19.2_darwin_amd64.tar.gz"
-      sha256 "0fc0b731524df57a917458e9f35901ecd0b2e29d22c211bafcb3a5c257a7f420"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.24.0/argus_1.24.0_darwin_amd64.tar.gz"
+      sha256 "78ec95a1c5da710df40c8e3128957f24cb173a435736b24507dbb86d763ddef4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.2/argus_1.19.2_linux_arm64.tar.gz"
-      sha256 "8ab5068f90d9c870f3018d8bc570f503c73073179da926fe80df1df17685d78d"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.24.0/argus_1.24.0_linux_arm64.tar.gz"
+      sha256 "20a87e8e3628a0df75fd80fbd7c85dc8d0fd83bbff9acacc2ba8f6f01635ed86"
     else
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.2/argus_1.19.2_linux_amd64.tar.gz"
-      sha256 "16b0e01a5909d52e462d8f002b75918956ebcdeb2d4f36c9ef937315633d4595"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.24.0/argus_1.24.0_linux_amd64.tar.gz"
+      sha256 "9fcaf193ff3d65409dbeb0a0952d82bac2379b7c077e22888209b4d788526bf7"
     end
   end
 
