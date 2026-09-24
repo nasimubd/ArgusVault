05974fc6da2293ceaaefee59b4e5957ed10419f7
MD Nasim <nasimubd21@gmail.com>
2026-09-24 12:31:03 +0800

    fix(release): publish binary-only v1.19.0 assets

--- Formula/argus.rb
+++ Formula/argus.rb
@@ -1,25 +1,25 @@
 class Argus < Formula
   desc "Go API gateway and session proxy for OpenAI-compatible agent traffic"
   homepage "https://github.com/nasimubd/ArgusVault"
-  license "LGPL-3.0-only"
+  license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.18.2/argus_1.18.2_darwin_arm64.tar.gz"
-      sha256 "887463dd97d468f83cc4554b4fe82b5e587e2fa5d94529b32faa4507dc43b601"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.0/argus_1.19.0_darwin_arm64.tar.gz"
+      sha256 "818b754765ae286e263b396e23c288dd09df737435fc26a18e1594d1fc1d85db"
     else
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.18.2/argus_1.18.2_darwin_amd64.tar.gz"
-      sha256 "20f884b200b52ff21cfe672919d08c9dde6c77b6fa00c32087bb59b169ef7e39"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.0/argus_1.19.0_darwin_amd64.tar.gz"
+      sha256 "7ae12b01a4025ad988af83b5decf567818ac29635a7f4c399899c5e5d02d0370"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.18.2/argus_1.18.2_linux_arm64.tar.gz"
-      sha256 "09c3af02f13ace1cb13dd977bca6c10591903c55c1f71b99cb608db28f83d747"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.0/argus_1.19.0_linux_arm64.tar.gz"
+      sha256 "3886c0d4cd457d2b9bcf10b40a4d0fda0c78e9b34310a725417ba1cf1c8990cb"
     else
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.18.2/argus_1.18.2_linux_amd64.tar.gz"
-      sha256 "a2d2c68e30cce476a54bf0ad48792fd01d97257a8cd4f1497a3c1335f3413a5c"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.0/argus_1.19.0_linux_amd64.tar.gz"
+      sha256 "45edd734905abbb7499d2bbc6a16d3833e77946036bb44bd3b3ca99c62c3805e"
     end
   end
 
