82211ab0bcc0a5e4eb73f1048b379ef36012e36f
MD Nasim <nasimubd21@gmail.com>
2026-09-24 12:37:39 +0800

    chore(release): update formula to v1.19.1

--- Formula/argus.rb
+++ Formula/argus.rb
@@ -5,21 +5,21 @@ class Argus < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.0/argus_1.19.0_darwin_arm64.tar.gz"
-      sha256 "818b754765ae286e263b396e23c288dd09df737435fc26a18e1594d1fc1d85db"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.1/argus_1.19.1_darwin_arm64.tar.gz"
+      sha256 "07106d8a3723dff0cda6e6e69531dad41f8b5c445901952db54abe248e292284"
     else
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.0/argus_1.19.0_darwin_amd64.tar.gz"
-      sha256 "7ae12b01a4025ad988af83b5decf567818ac29635a7f4c399899c5e5d02d0370"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.1/argus_1.19.1_darwin_amd64.tar.gz"
+      sha256 "d15499c2a58d12c841e0e004591201003d14338f3df834fcfbe1e95fab12bcb9"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.0/argus_1.19.0_linux_arm64.tar.gz"
-      sha256 "3886c0d4cd457d2b9bcf10b40a4d0fda0c78e9b34310a725417ba1cf1c8990cb"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.1/argus_1.19.1_linux_arm64.tar.gz"
+      sha256 "d18a5a11db1833acfb6c0bc602d57f9a12229f4b13bb7dc08a4f9e0efb8aae59"
     else
-      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.0/argus_1.19.0_linux_amd64.tar.gz"
-      sha256 "45edd734905abbb7499d2bbc6a16d3833e77946036bb44bd3b3ca99c62c3805e"
+      url "https://github.com/nasimubd/ArgusVault/releases/download/v1.19.1/argus_1.19.1_linux_amd64.tar.gz"
+      sha256 "8f13f3d6da90bfc00c05bdb22a4e50c0339cfc03ecf567f1674d5beaac56546a"
     end
   end
 
